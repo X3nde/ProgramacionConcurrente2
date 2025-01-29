243a63402933d0836cf8c89ee65a110f82481f34
Xende <xende.rodriguez@hotmail.com>
2025-01-29 22:48:10 +0100

    Base

---
+++ src/main/java/com/progcon/programacionconcurrente2/ProgramacionConcurrente2Application.java
@@ -0,0 +1,13 @@
+package com.progcon.programacionconcurrente2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionConcurrente2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramacionConcurrente2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/progcon/programacionconcurrente2/ProgramacionConcurrente2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progcon.programacionconcurrente2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionConcurrente2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
